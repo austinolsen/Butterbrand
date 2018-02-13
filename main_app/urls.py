750c59f0bd33a2fba51fd08e8d7a5e478f213bdc
Kenneth <kendomine@yahoo.com>
2018-02-13 15:39:19 -0800

    Created a simple frontend for landing page. Rerouted registration so no longer on posts, but old routes still need to be deleted.

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
-from views import index
+from django.conf import settings
+from . import views 
+from django.conf.urls.static import static
+
+app_name = 'main_app'
 
 urlpatterns = [
-    url(r'^$', index),
-]
+    url(r'^$', views.index, name ='index'),
+    url(r'^register/$', views.UserFormView.as_view(), name ='register'),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
